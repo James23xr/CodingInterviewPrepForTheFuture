2886e6e8e4e40930979fd06a10ae16509f9cdc40
James Fei-Baffoe <amoahbenedicta598@gmail.com>
2024-06-13 16:13:58 -0400

    Time: 75 ms (22.80%) | Memory: 16.8 MB (93.78%) - LeetSync

--- 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.py
+++ 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.py
@@ -1,6 +1,23 @@
-class Solution:
-    def maxChunksToSorted(self, arr: List[int]) -> int:
+class Solution(object):
+    def getMinValueFromRight(self, arr):
+        res = []
+        currMin = float('inf')
+        for el in reversed(arr):
+            currMin = min(el, currMin)
+            res.append(currMin)
 
-        acc = zip(accumulate(arr), accumulate(sorted(arr)))
+        return list(reversed(res))
+        
+    def maxChunksToSorted(self, arr):
+        minValueFromRight = self.getMinValueFromRight(arr)
+        currMax = 0
+        maxChunks = 1
 
-        return len(tuple(filter(lambda x: x[0] == x[1],acc)))
+        for i in range(len(arr)-1):
+            currMax = max(currMax, arr[i])
+            if currMax <= minValueFromRight[i+1]:
+                maxChunks += 1
+
+        return maxChunks
+
+
