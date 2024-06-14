6528fff543ba196b3ed318a27393f599966fcb80
James Fei-Baffoe <amoahbenedicta598@gmail.com>
2024-06-14 07:41:46 -0400

    Time: 47 ms (34.69%) | Memory: 16.9 MB (16.44%) - LeetSync

--- 682-baseball-game/baseball-game.py
+++ 682-baseball-game/baseball-game.py
@@ -5,10 +5,11 @@ class Solution:
             if op == "+":
                 stack.append(stack[-1]+stack[-2])
             elif op == "D":
-                stack.append(2 * stack[-1])
+                stack.append(stack[-1] * 2)
             elif op == "C":
                 stack.pop()
             else:
                 stack.append(int(op))
         return sum(stack)
+
     
