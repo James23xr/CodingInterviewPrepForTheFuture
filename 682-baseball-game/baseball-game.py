1f0a591bafbf3f31097f48a45847e779cad0f154
James Fei-Baffoe <amoahbenedicta598@gmail.com>
2024-06-15 14:54:42 -0400

    Time: 41 ms (75.64%) | Memory: 16.8 MB (16.07%) - LeetSync

--- 682-baseball-game/baseball-game.py
+++ 682-baseball-game/baseball-game.py
@@ -3,13 +3,12 @@ class Solution:
         stack = []
         for op in operations:
             if op == "+":
-                stack.append(stack[-1]+stack[-2])
+                stack.append(stack[-1] + stack[-2])
             elif op == "D":
-                stack.append(stack[-1] * 2)
+                stack.append(stack[-1] * 2) 
             elif op == "C":
                 stack.pop()
             else:
                 stack.append(int(op))
         return sum(stack)
 
-    
