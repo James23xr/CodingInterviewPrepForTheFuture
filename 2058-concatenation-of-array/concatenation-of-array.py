479c68815d483a080ca94742e20d5f345566b984
James Fei-Baffoe <amoahbenedicta598@gmail.com>
2024-06-15 14:47:34 -0400

    Time: 69 ms (62.24%) | Memory: 17 MB (15.50%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.py
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -2,6 +2,6 @@ class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
         ans = []
         for i in range(2):
-            for i in range(len(nums)):
-                ans.append(nums[i])
+            for j in range(len(nums)):
+                ans.append(nums[j])
         return ans
