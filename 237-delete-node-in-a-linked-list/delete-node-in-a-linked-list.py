17417880205a9cf63b3a5ecdeea8a54ca44de8ea
James Fei-Baffoe <amoahbenedicta598@gmail.com>
2024-06-25 00:15:20 -0400

    Time: 38 ms (59.82%) | Memory: 16.8 MB (11.93%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.py
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.py
@@ -11,7 +11,5 @@ class Solution:
         :rtype: void Do not return anything, modify node in-place instead.
         """
         node.val = node.next.val
-        node.next = node.next.next
-
-
+        node.next= node.next.next
         
