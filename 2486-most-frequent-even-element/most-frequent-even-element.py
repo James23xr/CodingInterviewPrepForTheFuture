baea7f5cc035fec90f2eeec1b71d7c9b0c1f0761
James Fei-Baffoe <amoahbenedicta598@gmail.com>
2024-06-30 18:12:40 -0400

    Time: 227 ms (42.35%) | Memory: 17 MB (42.57%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.py
+++ 2486-most-frequent-even-element/most-frequent-even-element.py
@@ -1,14 +1,12 @@
 class Solution:
     def mostFrequentEven(self, nums: List[int]) -> int:
         count = {}
-        mostf = -1
+        mostf = 0
         mostfe = -1
         for n in nums:
             if n%2 == 0: 
                 count[n] = 1+ count.get(n,0)
-                
                 if count[n] >mostf:
-                    mostf = max(count[n],mostf)
                     mostf = count[n]
                     mostfe = n
                 elif count[n] == mostf:
