37c67611b479e56ed9a36d4fa402680374d566d5
James Fei-Baffoe <amoahbenedicta598@gmail.com>
2024-07-06 00:05:43 -0400

    Time: 42 ms (43.02%) | Memory: 16.5 MB (34.58%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
@@ -3,9 +3,10 @@ class Solution:
         res = len(students)
         count = Counter(students)
         for s in sandwiches:
-            if count[s] > 0:
+            if count[s]>0:
                 res -= 1
                 count[s] -= 1
             else:
                 return res
         return res
+
