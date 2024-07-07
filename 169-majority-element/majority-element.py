fb411b824850491556b2db84e17af132e1a4882f
James Fei-Baffoe <amoahbenedicta598@gmail.com>
2024-07-07 09:05:29 -0400

    Time: 175 ms (36.66%) | Memory: 18 MB (53.66%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,8 +1,8 @@
 class Solution:
-    def majorityElement(self, ns: List[int]) -> int:
+    def majorityElement(self, nums: List[int]) -> int:
         r = 0
         c = 0
-        for n in ns:
+        for n in nums:
             if c == 0 or n == r:
                 c += 1
                 r = n
