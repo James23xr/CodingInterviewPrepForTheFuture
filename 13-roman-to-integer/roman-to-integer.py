ca21ae50e08aac087684fbb6c3f68046b367034d
James Fei-Baffoe <amoahbenedicta598@gmail.com>
2024-07-08 21:22:03 -0400

    Time: 40 ms (86.64%) | Memory: 16.6 MB (37.02%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,17 +1,16 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        Map = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        Map = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
         count = 0
         for i in range(len(s)):
-            if len(s) == 1:
-                return  Map[s[i]]
-            elif i == len(s)-1:
-                count += Map[s[i]]
-            elif Map[s[i]] < Map[s[i+1]]:
+            if i <(len(s)-1) and Map[s[i]] < Map[s[i+1]]:
                 count -= Map[s[i]]
             else:
                 count += Map[s[i]]
         return count
+
+      
+        
             
 
         
