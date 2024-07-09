6e0303ccdf71a9805639732612767845c7f07613
James Fei-Baffoe <amoahbenedicta598@gmail.com>
2024-07-08 22:26:25 -0400

    Time: 55 ms (14.79%) | Memory: 17 MB (76.57%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,7 +1,24 @@
 class Solution:
+    def AlphaNum(self,c):
+        return (ord("A")<= ord(c)<=ord("Z")or
+                ord("a")<=ord(c)<=ord("z")or
+                ord("0")<=ord(c)<=ord("9"))
+
     def isPalindrome(self, s: str) -> bool:
-        newstr = ''
-        for c in s:
-            if c.isalnum():
-                newstr += c.lower()
-        return newstr == newstr[::-1]
+        l = 0
+        r = len(s)-1
+        while l <r:
+            if not self.AlphaNum(s[l]):
+                l+=1
+            elif not self.AlphaNum(s[r]):
+                r-=1
+            else:
+                if s[l].lower() != s[r].lower():
+                    return False
+                l +=1
+                r-=1
+        return True
+
+    
+    
+        
