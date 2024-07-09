b51b6ebb1ee93766b66b4da6bb8bb2b5cffaa34a
James Fei-Baffoe <amoahbenedicta598@gmail.com>
2024-07-08 22:05:41 -0400

    Time: 42 ms (72.87%) | Memory: 17 MB (76.57%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,6 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        newstr = ""
+        newstr = ''
         for c in s:
             if c.isalnum():
                 newstr += c.lower()
