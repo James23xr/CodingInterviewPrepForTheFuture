c61e8c65df1c92a88086173dc9d55e7550278f35
James Fei-Baffoe <amoahbenedicta598@gmail.com>
2024-07-20 22:15:02 -0400

    Time: 99 ms (15.32%) | Memory: 21.9 MB (28.90%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -7,9 +7,9 @@ class Solution:
         for n,c in count.items():
             freq[c].append(n)
         res = []
-        for i in range(len(freq)-1,-1,-1):
-            for num in  freq[i]:
-                res.append(num)
+        for i in range(len(freq)-1,0,-1):
+            for n in freq[i]:
+                res.append(n)
                 if len(res) == k:
                     return res
 
