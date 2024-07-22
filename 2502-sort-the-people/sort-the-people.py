f381abb802062f9e5354c5645ae32accbc2b56da
James Fei-Baffoe <amoahbenedicta598@gmail.com>
2024-07-22 11:16:14 -0400

    Time: 89 ms (99.02%) | Memory: 17.2 MB (14.55%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,9 +1,9 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         h_n = {}
+        res = []
         for h,n in zip(heights,names):
             h_n[h] = n
-        res = []
         for h in reversed(sorted(heights)):
             res.append(h_n[h])
         return res
