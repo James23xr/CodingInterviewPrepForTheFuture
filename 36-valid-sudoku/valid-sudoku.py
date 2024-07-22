9759162862eb91031059852f2ed14cbc5abe8924
James Fei-Baffoe <amoahbenedicta598@gmail.com>
2024-07-22 17:50:11 -0400

    Time: 95 ms (39.01%) | Memory: 16.4 MB (97.11%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -7,7 +7,7 @@ class Solution:
             for c in range(9):
                 if board[r][c] == ".":
                     continue
-                elif (board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in square[r//3,c//3]):
+                if (board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in square[r//3,c//3]):
                     return False
                 cols[c].add(board[r][c])
                 rows[r].add(board[r][c])
