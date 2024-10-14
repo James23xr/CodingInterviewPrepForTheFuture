3f645e2e9ac68597c7738f7d0170fa04ab44784f
James Fei-Baffoe <amoahbenedicta598@gmail.com>
2024-10-14 14:01:00 -0400

    Time: 264 ms (39.16%) | Memory: 43.7 MB (24.35%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -1,16 +1,18 @@
-# Python3
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def minDepth(self, root):
-        # Define the depth-first search
+    def minDepth(self, root: Optional[TreeNode]) -> int:
         def dfs(root):
             if root is None:
                 return 0
-            # If only one of child is non-null, then go into that recursion.
             if root.left is None:
-                return 1 + dfs(root.right)
+                return dfs(root.right) + 1
             elif root.right is None:
-                return 1 + dfs(root.left)
-            # Both children are non-null, hence call for both children.
-            return 1 + min(dfs(root.left), dfs(root.right))
-
+                return dfs(root.left) + 1
+            return min(dfs(root.left),dfs(root.right)) + 1
         return dfs(root)
+        
