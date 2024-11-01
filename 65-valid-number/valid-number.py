e540ad656e7ba341363426b4cc1bfb0f90516366
James Fei-Baffoe <amoahbenedicta598@gmail.com>
2024-11-01 16:32:39 -0400

    Time: 3 ms (32.88%) | Memory: 16.6 MB (65.66%) - LeetSync

--- 65-valid-number/valid-number.py
+++ 65-valid-number/valid-number.py
@@ -5,27 +5,23 @@ class Solution:
         has_num = False
         has_dec = False
         has_exp = False
-        
         for i in range(n):
             char = s[i]
             if char.isdigit():
                 has_num = True
             elif char in ['+','-']:
-                if i > 0 and s[i-1] not in ['e','E']:
+                if i >0 and s[i-1] not in ['e','E']:
                     return False
             elif char == '.':
-                if  has_dec or has_exp:
+                if has_dec or has_exp:
                     return False
-                has_dec = True
+                has_dec =  True
             elif char in ['e','E']:
-                if has_exp or  not has_num:
+                if not has_num or has_exp:
                     return False
                 has_exp = True
                 has_num = False
             else:
                 return False
         return has_num
-                
-
-
         
