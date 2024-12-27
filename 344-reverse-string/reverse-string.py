a040d710987bff0c97d23acebc1875c644deaaf1
James Fei-Baffoe <amoahbenedicta598@gmail.com>
2024-12-27 08:57:44 -0500

    Time: 0 ms (100.00%) | Memory: 23 MB (9.83%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -3,12 +3,5 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        l = 0
-        r = len(s) -1
-        while l < r:
-            s[l], s[r] = s[r], s[l]
-            l += 1
-            r -= 1
-        return s
-
+        s.reverse()
         
