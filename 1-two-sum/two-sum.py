5bd3977a5f2a4d9d3fbb5395513d72accc9428d0
James Fei-Baffoe <amoahbenedicta598@gmail.com>
2025-01-08 23:04:48 -0500

    Time: 3 ms (53.60%) | Memory: 18.8 MB (25.31%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,9 +1,25 @@
+"""
+nums = [2,7,11,15]
+        |
+        diff = target - num
+        if diff in hashmap
+        then we can return [i,hashmap[diff]]
+        2:0
+        7:1
+        11:2
+        15:3
+target = 9
+return indices of two nums that add up to target
+
+"""
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen = {}
-        for i,e in enumerate(nums):
-            diff = target - e
+        for index,number in enumerate(nums):
+            diff = target - number
             if diff in seen:
-                return [seen[diff],i]
-            seen[e] = i
+                return [index,seen[diff]]
+            seen[number] = index
+        
         
