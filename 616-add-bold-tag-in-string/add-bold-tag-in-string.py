f89560e2dd690ff0625eb67a6996b33f3d716122
James Fei-Baffoe <amoahbenedicta598@gmail.com>
2025-01-10 16:18:41 -0500

    Time: 8 ms (79.58%) | Memory: 18 MB (43.55%) - LeetSync

--- 616-add-bold-tag-in-string/add-bold-tag-in-string.py
+++ 616-add-bold-tag-in-string/add-bold-tag-in-string.py
@@ -2,26 +2,19 @@ class Solution:
     def addBoldTag(self, s: str, words: List[str]) -> str:
         n = len(s)
         bold = [False] * n
-        
         for word in words:
             start = s.find(word)
             while start != -1:
-                for i in range(start, start + len(word)):
+                for i in range(start,start+len(word)):
                     bold[i] = True
-                    
-                start = s.find(word, start + 1)
-
+                start = s.find(word,start + 1)
         open_tag = "<b>"
-        close_tag = "</b>"
-        ans = []
-        
+        close_tag = '</b>'
+        res = []
         for i in range(n):
-            if bold[i] and (i == 0 or not bold[i - 1]):
-                ans.append(open_tag)
-                
-            ans.append(s[i])
-            
-            if bold[i] and (i == n - 1 or not bold[i + 1]):
-                ans.append(close_tag)
-        
-        return "".join(ans)
+            if bold[i] and (i==0 or  not bold[i-1]):
+                res.append(open_tag)
+            res.append(s[i])
+            if bold[i] and (i ==n-1 or not bold[i+1]):
+                res.append(close_tag)
+        return ''.join(res)
