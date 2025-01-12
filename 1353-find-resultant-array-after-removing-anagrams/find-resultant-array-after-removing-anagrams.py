76b6bcb49d173dec5a4ed58780275359e262078a
James Fei-Baffoe <amoahbenedicta598@gmail.com>
2025-01-11 22:23:04 -0500

    Time: 20 ms (25.15%) | Memory: 18.1 MB (10.15%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
@@ -1,18 +1,11 @@
 class Solution:
-    def removeAnagrams(self, words: List[str]) -> List[str]:
-            # Stack to hold the resultant words
+    def removeAnagrams(self, words: List[str]) -> List[str]:   
         stack = []
-        # To store the character frequency of the top word in the stack
         prev_freq = None
-
         for word in words:
-            # Generate character frequency as a tuple
             freq = tuple([word.count(char) for char in 'abcdefghijklmnopqrstuvwxyz'])
-            
-            # Compare with the previous frequency
             if freq != prev_freq:
                 stack.append(word)
-                prev_freq = freq  # Update the previous frequency
-        
+                prev_freq = freq  
         return stack
             
