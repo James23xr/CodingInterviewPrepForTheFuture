3097fdd17a53cdc6e1d385ad67c8a4fbf5a6f511
James Fei-Baffoe <amoahbenedicta598@gmail.com>
2025-01-11 21:25:10 -0500

    Time: 11 ms (74.96%) | Memory: 17.7 MB (53.45%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,13 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
+        #anagram word formed by rearranging the characters of one word to form another word using all the original characters once
+        if len(s) != len(t): # if string lengths are different it cannot be an anagram
             return False
-        count = defaultdict(int)
-        for i in range(len(s)):
-            count[s[i]] += 1
-            count[t[i]] -= 1
-        for char in count:
+        count = defaultdict(int) #defaultdict automates creation of key with value  of zero if key does not 
+        for i in range(len(s)): #since they are same length we an use one length of one string to iterate through both
+            count[s[i]] += 1 #increase char count by 1
+            count[t[i]] -= 1 #decrease char count by 1
+        for char in count: #go through dictionary
             if count[char] != 0:
-                return False
-        return True
+                return False #cannot be anagram
+        return True #all charcounts are 0
         
