e51463e257fcbe1f902bcbe9cc689fa14c1ca35a
James Fei-Baffoe <amoahbenedicta598@gmail.com>
2025-01-14 11:04:17 -0500

    Time: 65 ms (53.58%) | Memory: 18.3 MB (9.73%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -3,7 +3,7 @@ class Solution:
         count = defaultdict(int)
         for char in s:
             count[char] += 1
-        for i, char in enumerate(s):
+        for i,char in enumerate(s):
             if count[char] == 1:
                 return i
         return -1
