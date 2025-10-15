1b168020778f819ae3f83551ffe61a831f4eefa6
James Fei-Baffoe <amoahbenedicta598@gmail.com>
2025-10-15 00:47:52 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (53.63%) - LeetSync

--- 228-summary-ranges/summary-ranges.py
+++ 228-summary-ranges/summary-ranges.py
@@ -1,16 +1,16 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
-        result = []
         i = 0
+        res = []
         while i < len(nums):
             start = nums[i]
-            while i < len(nums)-1 and nums[i] +1 == nums[i+1]:
+            while i < len(nums) -1 and nums[i] +1 == nums[i+1]:
                 i+=1
             if start != nums[i]:
-                result.append(str(start) + '->'+str(nums[i]))
-            else:
-                result.append(str(start))
+                res.append(str(start) + '->' + str(nums[i]))
+            elif start == nums[i]:
+                res.append(str(start))
             i+=1
-        return result
+        return res
         
         
